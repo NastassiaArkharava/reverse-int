c4e78cbde9ab165e2d452f23127b0c15ce4b14c3
Nastassia Arkharava <nastassiaarkharava@gmail.com>
2023-01-02 19:24:46 +0300

    feat: implement function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+	let stringNum = Math.abs(n).toString();
+	let reverseNum = '';
+	
+	for (let i = stringNum.length - 1; i >= 0; i--) {
+		reverseNum += stringNum[i];
+	}
+
+  return reverseNum;
 }
